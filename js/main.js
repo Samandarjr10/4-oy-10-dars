26f6aaa4853ca9db52877dea152efdf267084f44
Samandarjr10 <samandarjr123@gmail.com>
2021-12-16 22:24:33 +0500

    second way

--- js/main.js
+++ js/main.js
@@ -6,36 +6,6 @@ var elSearchHeading = document.querySelector("#search_result");
 var elTemleteItem = document.querySelector("#movie_item_template").content
 
 
-function renderMovies(moviesArr, wrapper) {
-    var movieItem = document.createDocumentFragment()
-
-    moviesArr.forEach(item => {
-        var movieLi = elTemleteItem.cloneNode(true);
-        movieLi.querySelector("#movie_img").src = item.Poster;
-        movieLi.querySelector("#movie_heading").textContent = item.Title;
-        movieLi.querySelector("#movie_year").textContent = item.Year;
-        movieLi.querySelector("#movie_imdbid").textContent = item.imdbID;
-        movieItem.appendChild(movieLi);
-    })
-    wrapper.innerHTML = null;
-    wrapper.appendChild(movieItem);
-}
-
-
-async function fetchData() {
-    var response = await fetch("https://www.omdbapi.com/?apikey=73119f16&s=iron&page=4")
-
-    var data = await response.json()
-    renderMovies(data.Search, elList)
-}
-
-fetchData()
-// bu yol bilan searchchi ishlatishshi bilomadm!
-
-
-// -------------------------
-// var twoMovies = movies;
-
 // function renderMovies(moviesArr, wrapper) {
 //     var movieItem = document.createDocumentFragment()
 
@@ -49,36 +19,63 @@ fetchData()
 //     })
 //     wrapper.innerHTML = null;
 //     wrapper.appendChild(movieItem);
-
-//     elSearchHeading.textContent = `Search result: ${moviesArr.length}`;
 // }
 
-// renderMovies(twoMovies, elList);
+
+// async function fetchData() {
+//     var response = await fetch("https://www.omdbapi.com/?apikey=73119f16&s=iron&page=4")
+
+//     var data = await response.json()
+//     renderMovies(data.Search, elList)
+// }
+
+// fetchData()
+// bu yol bilan searchchi ishlatishshi bilomadm!
 
 
-// elForm.addEventListener("click", (evt) => {
-//     evt.preventDefault();
+// -------------------------
+var twoMovies = movies;
 
-//     var pushNewMoviesArr = [];
-//     var newImg = document.createElement("img")
-//     newImg.setAttribute("href", "")
+function renderMovies(moviesArr, wrapper) {
+    var movieItem = document.createDocumentFragment()
 
-//     var searchInput = elInputTitle.value.trim();
-//     var inputKeyTitle = new RegExp(searchInput, "gi");
-//     twoMovies.forEach(movieItem => {
-//         var searchInfo = movieItem.Title.match(inputKeyTitle)
-//         if (searchInfo) {
-//             pushNewMoviesArr.push(movieItem)
-//         }
-//     })
-//     if (pushNewMoviesArr.length > 0) {
-//         elList.innerHTML = null;
-//         renderMovies(pushNewMoviesArr, elList);    
-//     }else {
-//         elList.innerHTML = "Kino yo'q"
+    moviesArr.forEach(item => {
+        var movieLi = elTemleteItem.cloneNode(true);
+        movieLi.querySelector("#movie_img").src = item.Poster;
+        movieLi.querySelector("#movie_heading").textContent = item.Title;
+        movieLi.querySelector("#movie_year").textContent = item.Year;
+        movieLi.querySelector("#movie_imdbid").textContent = item.imdbID;
+        movieItem.appendChild(movieLi);
+    })
+    wrapper.innerHTML = null;
+    wrapper.appendChild(movieItem);
 
-//     }
-// })
+    elSearchHeading.textContent = `Search result: ${moviesArr.length}`;
+}
+
+renderMovies(twoMovies, elList);
 
 
+elForm.addEventListener("click", (evt) => {
+    evt.preventDefault();
 
+    var pushNewMoviesArr = [];
+    var newImg = document.createElement("img")
+    newImg.setAttribute("href", "")
+
+    var searchInput = elInputTitle.value.trim();
+    var inputKeyTitle = new RegExp(searchInput, "gi");
+    twoMovies.forEach(movieItem => {
+        var searchInfo = movieItem.Title.match(inputKeyTitle)
+        if (searchInfo) {
+            pushNewMoviesArr.push(movieItem)
+        }
+    })
+    if (pushNewMoviesArr.length > 0) {
+        elList.innerHTML = null;
+        renderMovies(pushNewMoviesArr, elList);    
+    }else {
+        elList.innerHTML = "Kino yo'q"
+
+    }
+})
